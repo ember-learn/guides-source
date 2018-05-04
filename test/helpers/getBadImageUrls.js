211ea545c5f2700e73fe228bc862a87f7f888c6e
Chris Manson <chris@stonecircle.io>
2018-05-04 15:40:05 +0100

    catching trailling slashes on images

--- test/helpers/getBadImageUrls.js
+++ test/helpers/getBadImageUrls.js
@@ -3,7 +3,7 @@ const _ = require('lodash');
 const checkIfPathExists = require('./checkIfPathExists');
 
 module.exports = function getBadRelativeUrlsForFile(mdFile) {
-  return _.chain(mdFile.links)
+  const badImages = _.chain(mdFile.links)
     .filter(link => link.endsWith('.png') || link.endsWith('.gif'))
     .map((link) => {
       // ignore external images
@@ -24,4 +24,16 @@ module.exports = function getBadRelativeUrlsForFile(mdFile) {
     })
     .compact()
     .value();
+
+  const traillingSlashImages = _.chain(mdFile.links)
+    .filter(link => link.endsWith('.png/') || link.endsWith('.gif/'))
+    .map(link => ({
+      fileToFix: mdFile.filepath,
+      badImageLink: link,
+      reason: 'extra trailing slash on image',
+    }))
+    .compact()
+    .value();
+
+  return _.concat(badImages, traillingSlashImages);
 };
