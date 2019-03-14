89cbfad07346ba84bf45178bbafde306211d30d2
Chris Manson <mansona@users.noreply.github.com>
2019-03-14 13:26:25 +0000

    Preview banner (#599)
    * adding info-banner
    
    * fixing broken pages.yml

--- config/environment.js
+++ config/environment.js
@@ -56,6 +56,14 @@ module.exports = function(environment) {
         }
       },
     ],
+    infoBanner: {
+      content: `Thanks for taking the Octane preview for a test drive! Visit
+<a href="https://emberjs.com" target="_blank" rel="noopener">emberjs.com/octane</a>
+for more info and ways you can help out.
+These preview guides URLs are subject to change, so point your bookmarks at that landing page.
+If you are looking for the latest stable release of Ember, please instead visit
+<a href="https://guides.emberjs.com" target="_blank" rel="noopener">guides.emberjs.com</a>.`
+    },
   };
 
   if (environment === 'development') {
