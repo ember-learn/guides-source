41f9d53faad4a5df9014709a48b70f5e9e7dd8e8
billybonks <sebastienstettler@gmail.com>
2019-06-24 23:50:45 +0800

    [REFACTOR] Remove depreaction deprecate-router-events

--- app/router.js
+++ app/router.js
@@ -12,9 +12,11 @@ const Router = EmberRouter.extend({
   metrics: service(),
   fastboot: service(),
 
-  didTransition() {
+  init() {
     this._super(...arguments);
-    this._trackPage();
+    this.on('routeDidChange', () => {
+      this._trackPage();
+    })
   },
 
   _trackPage() {
