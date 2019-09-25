eb977bc1a90853c759f2c62e24fa6b633f029654
Chris Garrett <cgarrett@linkedin.com>
2019-09-25 17:59:43 -0400

    [FEAT] Adds the ability to have nested navigation
    This PR updates Guidemaker and it's Ember template to add nested
    navigation to the guides. Since we already had some pages that had
    "nested" docs (the cookbooks back in 1.10-1.12), I also had to make some
    updates to them to make sure they don't clash with the new system and
    its constraints. The updates move some pages about and add them all to
    the ToC, but otherwise don't change them (original URLs stay intact).
    
    Also refactored a test to take nested guides into account (this will
    become important later).

--- tests/acceptance/side-bar-links-test.js
+++ tests/acceptance/side-bar-links-test.js
@@ -3,32 +3,33 @@ import { visit, find } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import { get } from '@ember/object';
 
+async function visitPages(pages, assert) {
+  for (let page of pages) {
+    if (page.skipToc) continue;
+
+    if (get(page, 'pages')) {
+      await visitPages(get(page, 'pages'), assert);
+    } else {
+      let url = get(page, 'url');
+      let title = get(page, 'title');
+
+      await visit(`/release/${url}`);
+
+      let titleSearch = await find(`h1`);
+      assert.equal(titleSearch.innerText, title, title);
+    }
+  }
+}
+
 module('Acceptance | side bar links', function(hooks) {
   setupApplicationTest(hooks);
 
   test('release links go to correct page', async function(assert) {
-		await visit('/release');
+    await visit('/release');
 
-		let store = this.owner.lookup('service:store');
+    let store = this.owner.lookup('service:store');
     let pages = await store.peekAll('page');
 
-		await pages.reduce(async (prev, section) => {
-			await prev;
-
-			if (section.skipToc) return Promise.resolve();
-
-			return section.get('pages').reduce(async (prev, page) => {
-				await prev;
-
-				let url = get(page, 'url');
-				let title = get(page, 'title');
-
-				await visit(`/release/${url}`);
-
-				let titleSearch = await find(`h1`);
-				assert.equal(titleSearch.innerText, title);
-
-			}, Promise.resolve());
-		}, Promise.resolve());
+    await visitPages(pages.toArray(), assert);
   });
 });
