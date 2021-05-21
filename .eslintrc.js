e2d37d49b9098c8262c1e09f8e81decdda1cb4f6
Isaac Lee <ijlee2@users.noreply.github.com>
2021-05-21 07:15:46 +0200

    Updated ember-source to v3.24.4

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,42 +7,40 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
     ecmaFeatures: {
-      legacyDecorators: true
-    }
+      legacyDecorators: true,
+    },
   },
-  plugins: [
-    'ember'
-  ],
+  plugins: ['ember'],
   extends: [
     'eslint:recommended',
-    'plugin:ember/recommended'
+    'plugin:ember/recommended',
+    'plugin:prettier/recommended',
   ],
   env: {
-    browser: true
+    browser: true,
   },
-  rules: {
-    'ember/no-jquery': 'error'
-  },
+  rules: {},
   overrides: [
     // node files
     {
       files: [
         '.eslintrc.js',
+        '.prettierrc.js',
         '.template-lintrc.js',
         'ember-cli-build.js',
         'testem.js',
         'blueprints/*/index.js',
         'config/**/*.js',
         'lib/*/index.js',
-        'server/**/*.js'
+        'server/**/*.js',
       ],
       parserOptions: {
-        sourceType: 'script'
+        sourceType: 'script',
       },
       env: {
         browser: false,
-        node: true
-      }
+        node: true,
+      },
     },
     // node tests
     {
@@ -51,16 +49,19 @@ module.exports = {
       ],
       env: {
         node: true,
-        mocha: true
+        mocha: true,
       },
       parserOptions: {
-        ecmaVersion: 2018
+        ecmaVersion: 2018,
       },
       rules: {
         "func-names": 0,
         "prefer-arrow-callback": 0,
-        "no-unused-expressions": 0
+        "no-unused-expressions": 0,
+        // this can be removed once the following is fixed
+        // https://github.com/mysticatea/eslint-plugin-node/issues/77
+        'node/no-unpublished-require': 'off',
       },
-    }
-  ]
+    },
+  ],
 };
