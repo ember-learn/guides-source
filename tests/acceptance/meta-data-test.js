2f50a5d5acb3eb391854ecc23f3b57380a62842c
Chris Manson <chris@manson.ie>
2021-08-20 10:48:08 +0100

    fix tests because of updated template

--- tests/acceptance/meta-data-test.js
+++ tests/acceptance/meta-data-test.js
@@ -7,24 +7,27 @@ module('Acceptance | meta data', function(hooks) {
 
   test('meta data title and description', async function(assert) {
     await visit('/release/');
-    await click('[data-test-toc-title="Routing"]')
-    await click('[data-test-toc-link="Defining Your Routes"]')
 
+    await click('[data-test-toc-link="Routing"]');
+    assert.equal(currentURL(), '/release/routing');
+
+    // look up the og:title instead of title because testem modifies title https://github.com/testem/testem/issues/195
+    assert.dom('head meta[property="og:title"]', document)
+      .hasAttribute('content', 'Introduction - Routing - Ember Guides');
+    assert.dom('head meta[name="description"]', document)
+      .hasAttribute('content', 'Imagine we are writing a web app for managing a blog. At any given time, we should be able to answer questions like What post are they looking at? and Are they editing it? In Ember.js, the answer to these questions is determined by the URL. \n\nThe URL can be...');
+
+
+    await click('[data-test-toc-link="Defining Your Routes"]');
     assert.equal(currentURL(), '/release/routing/defining-your-routes');
 
-    // lookup title from service because testem modifies title https://github.com/testem/testem/issues/195
-    let headData = this.owner.lookup('service:head-data');
-    let title = headData.title;
+    assert.dom('head meta[property="og:title"]', document)
+      .hasAttribute('content', 'Defining Your Routes - Routing - Ember Guides');
+    assert.dom('head meta[name="description"]', document)
+      .hasAttribute('content', `When your application starts, the router matches the current URL to the routes that you've defined. The routes, in turn, are responsible for displaying templates, loading data, and setting up application state.
 
-    let description = document.head.querySelector('meta[name="description"]');
+To define a route, run
 
-    assert.ok(title);
-    assert.ok(description);
-
-    assert.equal(title,
-      'Defining Your Routes - Routing - Ember Guides');
-
-    const descriptionStart = 'When your application starts, the router matches the current URL to the routes';
-    assert.ok(description.content.startsWith(descriptionStart), `${description.content} should start with ${descriptionStart}`);
+ember generate...`);
   });
 });
--- tests/acceptance/table-of-contents-test.js
+++ tests/acceptance/table-of-contents-test.js
@@ -7,7 +7,7 @@ module('Acceptance | table of contents', function(hooks) {
 
   test('navigation by TOC', async function(assert) {
     await visit('/v2.17.0/');
-    await click('[data-test-toc-title="Routing"]')
+    await click('[data-test-toc-link="Routing"]')
     await click('[data-test-toc-link="Defining Your Routes"]')
     assert.equal(currentURL(), '/v2.17.0/routing/defining-your-routes');
   });
