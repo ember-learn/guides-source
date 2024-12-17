2d3ce8dae7053f40042c5e78a30ab12dd5fa8517
Katie Gengler <katie@kmg.io>
2024-12-17 13:51:14 -0500

    Add the trailing-history location locally with support for query params until https://github.com/empress/guidemaker/pull/117 is merged and released

---
+++ app/locations/trailing-history.js
@@ -0,0 +1,19 @@
+/* TODO: Remove this after the upstream PR to allow query params with the trailing history location is merged
+ *   https://github.com/empress/guidemaker/pull/117
+ */
+
+/* eslint-disable ember/no-classic-classes, prettier/prettier */
+import HistoryLocation from '@ember/routing/history-location';
+
+export default HistoryLocation.extend({
+  formatURL() {
+    let url = this._super(...arguments);
+    return formatURL(url);
+  },
+});
+
+export function formatURL(url) {
+  let modifiedURL = new URL(url, 'http://example.com');
+  modifiedURL.pathname += '/';
+  return `${modifiedURL.pathname}${modifiedURL.search}${modifiedURL.hash}`;
+}
--- app/routes/application.js
+++ app/routes/application.js
@@ -11,7 +11,7 @@ export default class ApplicationRoute extends GuideMakerApp {
   model(params, ...rest) {
     let { feature_flags } = params;
     let overrides = Object.fromEntries(
-      feature_flags?.split(',').map((flag) => [flag, true]) ?? []
+      feature_flags?.split(',').map((flag) => [flag, true]) ?? [],
     );
     this.features.setupFeatures(Object.assign({}, FEATURES, overrides));
     return super.model(params, ...rest);
